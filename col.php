78b875d5ce3dc4f8510f03352ceb20342bb3afe8
caraclarke <cara.clarke8@gmail.com>
2016-04-21 15:09:56 -0400

    rough template 1 together

--- col.php
+++ col.php
@@ -1,38 +1,90 @@
 <?php
-echo '<style>
-    /* Column Drop Layout Pattern CSS */
-	@media only screen and (max-width: 450px) {
-	    td[class="column"] {
-	        display: block;
-            width: 100%;
-            -moz-box-sizing: border-box;
-            -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-	    }
-	}
-</style>
-<table cellpadding="0" cellspacing="0">
-    <tr>
-        <td class="column" width="600" colspan="2" align="left" valign="top" style="padding: 10px 20px 20px 20px; background: #999; font-family: arial,sans-serif; font-size: 14px; line-height: 18px; color: #000001;">
-        	<h1>Column 1</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris. Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus. Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus. Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci. Fusce id felis. Vivamus sollicitudin metus eget eros.
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td class="column" width="300" align="left" valign="top" style="padding: 20px; background: #666; font-family: arial,sans-serif; font-size: 14px; line-height: 18px; color: #fff;">
-            <h2 style="border: 0;">Column 2</h2>
-            <p>
-                Nunc tempus felis vitae urna. Vivamus porttitor, neque at volutpat rutrum, purus nisi eleifend libero, a tempus libero lectus feugiat felis. Morbi diam mauris, viverra in, gravida eu, mattis in, ante. Morbi eget arcu. Morbi porta, libero id ullamcorper nonummy, nibh ligula pulvinar metus, eget consectetuer augue nisi quis lacus. Ut ac mi quis lacus mollis aliquam. Curabitur iaculis tempus eros. Curabitur vel mi sit amet magna malesuada ultrices. Ut nisi erat, fermentum vel, congue id, euismod in, elit. Fusce ultricies, orci ac feugiat suscipit, leo massa sodales velit, et scelerisque mi tortor at ipsum. Proin orci odio, commodo ac, gravida non, tristique vel, tellus. Pellentesque nibh libero, ultricies eu, sagittis non, mollis sed, justo. Praesent metus ipsum, pulvinar pulvinar, porta id, fringilla at, est.
-            </p>
-        </td>
-        <td class="column" width="300" align="left" valign="top" style="padding: 20px; background: #333; color: #fff; font-family: arial,sans-serif; font-size: 14px; line-height: 18px;">
-            <h2 style="border: 0;">Column 3</h2>
-            <p>
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In posuere felis nec tortor. Pellentesque faucibus. Ut accumsan ultricies elit. Maecenas at justo id velit placerat molestie. Donec dictum lectus non odio. Cras a ante vitae enim iaculis aliquam. Mauris nunc quam, venenatis nec, euismod sit amet, egestas placerat, est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras id elit. Integer quis urna. Ut ante enim, dapibus malesuada, fringilla eu, condimentum quis, tellus. Aenean porttitor eros vel dolor. Donec convallis pede venenatis nibh. Duis quam. Nam eget lacus. Aliquam erat volutpat. Quisque dignissim congue leo.
-            </p>
-        </td>
-    </tr>
-</table>';
+echo '<tr>
+    <td align="center" valign="top" style="padding: 10px;">
+        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+            <tr>
+                <!-- Column : BEGIN -->
+                <td width="33.33%" class="stack-column-center">
+                    <table cellspacing="0" cellpadding="0" border="0">
+                        <tr>
+                            <td style="padding: 10px; text-align: center">
+                                <img src="http://placehold.it/170" width="170" height="170" alt="alt_text" border="0" class="fluid">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0 10px 10px; text-align: left;" class="center-on-narrow">
+                                Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                            
+                            
+                                <table cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="float:left;">
+                                    <tr>
+                                        <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                            <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                    &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                                
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <!-- Column : END -->
+                <!-- Column : BEGIN -->
+                <td width="33.33%" class="stack-column-center">
+                    <table cellspacing="0" cellpadding="0" border="0">
+                        <tr>
+                            <td style="padding: 10px; text-align: center">
+                                <img src="http://placehold.it/170" width="170" height="170" alt="alt_text" border="0" class="fluid">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0 10px 10px; text-align: left;" class="center-on-narrow">
+                                Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                            
+                                <table cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="float:left;">
+                                    <tr>
+                                        <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                            <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                    &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <!-- Column : END -->
+                <!-- Column : BEGIN -->
+                <td width="33.33%" class="stack-column-center">
+                    <table cellspacing="0" cellpadding="0" border="0">
+                        <tr>
+                            <td style="padding: 10px; text-align: center">
+                                <img src="http://placehold.it/170" width="170" height="170" alt="alt_text" border="0" class="fluid">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0 10px 10px; text-align: left;" class="center-on-narrow">
+                                Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                            
+                                <table cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="float:left;">
+                                    <tr>
+                                        <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                            <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                    &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <!-- Column : END -->
+            </tr>
+        </table>
+    </td>
+</tr>';
 ?>
---
+++ header.php
@@ -0,0 +1,32 @@
+<?php
+echo '<table bgcolor="#222222" cellpadding="0" cellspacing="0" border="0" height="100%" width="100%" style="border-collapse:collapse;"><tr><td valign="top">
+    <center style="width: 100%;">
+
+        <!-- Visually Hidden Preheader Text : BEGIN -->
+        <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
+            (Optional) This text will appear in the inbox preview, but not the email body.
+        </div>
+        <!-- Visually Hidden Preheader Text : END -->
+
+        <!-- Email Header : BEGIN -->
+        <table align="left" width="600" class="email-container">
+			<tr>
+				<td style="padding: 20px 0; text-align: center">
+					<img src="http://placehold.it/200x50" width="200" height="50" alt="alt_text" border="0">
+				</td>
+			</tr>
+        </table>
+				
+				<table align="center" width="600" class="email-container">
+					<tr>
+						<td style="padding: 5px; text-align: center; font-family: sans-serif; font-size: 30px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
+								Maecenas sed ante pellentesque.
+						</td>
+					</tr>
+					<tr>
+							<td style="padding: 25px 5px; text-align: center; font-family: sans-serif; font-size: 20px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
+									Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+							</td>
+					</tr>
+				</table>';
+?>
