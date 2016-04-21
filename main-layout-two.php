bb303779ffd6e0ab14df348fecec0a1a5bb27992
caraclarke <cara.clarke8@gmail.com>
2016-04-21 15:45:07 -0400

    rough layout two done

---
+++ main-layout-two.php
@@ -0,0 +1,60 @@
+<?php
+  echo '<tr>
+      <td align="center" valign="top" style="padding: 10px;">
+          <table cellspacing="0" cellpadding="0" border="0" width="100%">
+              <tr>
+                  <!-- Column : BEGIN -->
+                  <td class="stack-column-center">
+                      <table cellspacing="0" cellpadding="0" border="0">
+                          <tr>
+                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0 10px 10px; text-align: left;" class="center-on-narrow">
+                              <h1>heyhey</h1>
+                                  Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                                  <br><br>
+                                  <table cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="float:left;">
+                                      <tr>
+                                          <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                              <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                      &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                  </a>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </td>
+                          </tr>
+                          
+                          <tr>
+                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0 10px 10px; text-align: left;" class="center-on-narrow">
+                              <h1>heyhey</h1>
+                                  Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                                  <br><br>
+                                  <table cellspacing="0" cellpadding="0" border="0" class="center-on-narrow" style="float:left;">
+                                      <tr>
+                                          <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                              <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                      &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                  </a>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+                  <!-- Column : END -->
+                  <!-- Column : BEGIN -->
+                  <td class="stack-column-center">
+                      <table cellspacing="0" cellpadding="0" border="0">
+                          <tr>
+                              <td style="padding: 10px; text-align: center">
+                                  <img src="http://placehold.it/300" width="300" height="475" alt="alt_text" border="0" class="fluid">
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+                  <!-- Column : END -->
+              </tr>
+          </table>
+      </td>
+  </tr>';
+?>
