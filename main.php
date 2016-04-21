344ff67bcb156cb864d5bfd67f174d46d2cfaf73
caraclarke <cara.clarke8@gmail.com>
2016-04-21 14:38:41 -0400

    all files being referenced in main file

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Emails</title>
+  </head>
+  <body>
+    <?php include 'drip.php';?>
+    <?php include 'splash.php';?>
+    <?php include 'col.php';?>
+    <?php include 'footer.php';?>
+  </body>
+</html>
