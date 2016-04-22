9e6eac897660117597957e75447dcd482f9dee23
caraclarke <cara.clarke8@gmail.com>
2016-04-22 11:55:17 -0400

    layout one component spacing in files changed so easier to read

--- header.php
+++ header.php
@@ -1,38 +1,31 @@
 <?php
-echo '<table bgcolor="#fff" cellpadding="0" cellspacing="0" border="0" height="100%" width="100%" style="border-collapse:collapse;"><tr><td valign="top">
-    <center style="width: 100%;">
-
-        <!-- Visually Hidden Preheader Text : BEGIN -->
-        <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
-            <!-- (Optional) This text will appear in the inbox preview, but not the email body. -->
-        </div>
-        <!-- Visually Hidden Preheader Text : END -->
-
-        <!-- Email Header : BEGIN -->
-        <table align="left" width="100%" class="email-container">
-			<tr>
-				<td style="padding: 20px 0; text-align: center">
-					<img style="margin-right:40%;" src="http://placekitten.com/200/50" width="200" height="50" alt="alt_text" border="0">
-				</td>
-			</tr>
-        </table>
+echo '
+<!-- Email Header : BEGIN -->
+<table align="left" width="100%" class="email-container">
+  <tr>
+    <td style="padding: 20px 0; text-align: center">
+      <img style="margin-right:40%;" src="http://placekitten.com/200/50" width="200" height="50" alt="alt_text" border="0">
+    </td>
+  </tr>
+</table>
 				
-				<table align="center" width="600" class="email-container">
-					<tr>
-						<td style="padding: 5px; text-align: center; font-family: sans-serif; font-size: 30px; mso-height-rule: exactly; line-height: 20px; color: #000;">
-								Pelt around the house.
-						</td>
-					</tr>
-					<tr>
-							<td class="leftOnMin" style="padding: 25px 5px; text-align: center; font-family: sans-serif; font-size: 20px; mso-height-rule: exactly; line-height: 20px; color: #000; border-bottom: 3px solid blue">
-									 And up and down stairs chasing phantoms get video posted to internet for chasing red dot
-							</td>
-					</tr>
+<table align="center" width="600" class="email-container">
+	<tr>
+  	<td style="padding: 5px; text-align: center; font-family: sans-serif; font-size: 30px; mso-height-rule: exactly; line-height: 20px; color: #000;">
+  		Pelt around the house.
+  	</td>
+	</tr>
+	<tr>
+  	<td class="leftOnMin" style="padding: 25px 5px; text-align: center; font-family: sans-serif; font-size: 20px; mso-height-rule: exactly; line-height: 20px; color: #000; border-bottom: 3px solid blue">
+  		And up and down stairs chasing phantoms get video posted to internet for chasing red dot
+  	</td>
+	</tr>
           
-          <tr>
-							<td class="leftOnMin" style="padding: 25px 5px; text-align: center; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #000;">
-									All of a sudden cat goes crazy pee in human\'s bed until he cleans the litter box burrow under covers climb leg. Sun bathe cats go for world domination, the dog smells bad. Chase imaginary bugs chase the pig around the house for white cat sleeps on a black shirt and sit on the laptop.
-							</td>
-					</tr>
-				</table>';
+  <tr>
+  	<td class="leftOnMin" style="padding: 25px 5px; text-align: center; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #000;">
+  		All of a sudden cat goes crazy pee in human\'s bed until he cleans the litter box burrow under covers climb leg. Sun bathe cats go for world domination, the dog smells bad. Chase imaginary bugs chase the pig around the house for white cat sleeps on a black shirt and sit on the laptop.
+  	</td>
+	</tr>
+</table>
+';
 ?>
