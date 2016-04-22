91d6ec93d6d0194fe73a669e998108fed42dc3df
caraclarke <cara.clarke8@gmail.com>
2016-04-22 09:21:49 -0400

    footer layout roughly updated

--- footer.php
+++ footer.php
@@ -1,13 +1,25 @@
 <?php
   echo '<table align="center" width="600" class="email-container">
+  <tr>
+    <td style="padding: 20px 0px 5px 0px; text-align: center">
+      <img src="http://placekitten.com/200/50" width="200" height="50" alt="alt_text" border="0">
+    </td>
+  </tr>
       <tr>
-          <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #888888;">
-              <webversion style="color:#cccccc; text-decoration:underline; font-weight: bold;">View as a Web Page</webversion>
-              <br><br>
-              Company Name<br><span class="mobile-link--footer">123 Fake Street, SpringField, OR, 97477 US</span><br><span class="mobile-link--footer">(123) 456-7890</span>
-              <br><br> 
-              <unsubscribe style="color:#888888; text-decoration:underline;">unsubscribe</unsubscribe>
+          <td style="width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #888888;">
+              <span class="mobile-link--footer">Edit your email settings</span> / <unsubscribe style="color:#888888; text-decoration:underline;">unsubscribe</unsubscribe><br>
+              <span class="mobile-link--footer">123 Fake Street, SpringField, OR, 97477 US</span><br>
+              Copyright 2015 Codiscope<br>
+              <span class="mobile-link--footer">Privary Policy</span>
           </td>
       </tr>
+    <tr>
+      <td style="text-align:center;">
+        <img style="padding:10px;" src="http://placekitten.com/32/32" width="32" height="32" alt="alt_text" border="0">
+        <img style="padding:10px;" src="http://placekitten.com/32/32" width="32" height="32" alt="alt_text" border="0">
+        <img style="padding:10px;" src="http://placekitten.com/32/32" width="32" height="32" alt="alt_text" border="0">
+        <img style="padding:10px;" src="http://placekitten.com/32/32" width="32" height="32" alt="alt_text" border="0">
+      </td>
+    </tr>
   </table>';
 ?>
