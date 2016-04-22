baac37b09983955858ab484e51fb92f9b8cf2d22
caraclarke <cara.clarke8@gmail.com>
2016-04-22 10:25:34 -0400

    spacing updated based on templates

--- landing-image.php
+++ landing-image.php
@@ -7,18 +7,18 @@ echo '<tr>
 <!-- Hero Image, Flush : END -->
 
 <!-- 1 Column Text : BEGIN -->
-<tr style="background-color:#000;">
-<td style="padding: 10px 10px 0px 10px; text-align: left; font-family: sans-serif; font-size: 25px; mso-height-rule: exactly; line-height: 25px; color: #fff;">
-   Lay on arms while you\'re using the keyboard.
-</td>
-</tr>
-<tr style="background-color:#000;">
-<td style="padding: 10px 10px 0px 10px; text-align: left; font-family: sans-serif; font-size: 20px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
-    I\'m getting hungry
-</td>
-</tr>
-<tr style="background-color:#000;">
-    <td style="padding: 10px 10px 25px 10px; text-align: left; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
+  <tr style="background-color:#000;">
+  <td style="padding: 25px 25px 0px 25px; text-align: left; font-family: sans-serif; font-size: 25px; mso-height-rule: exactly; line-height: 25px; color: #fff;">
+     Lay on arms while you\'re using the keyboard.
+  </td>
+  </tr>
+  <tr style="background-color:#000;">
+  <td style="padding: 10px 25px 0px 25px; text-align: left; font-family: sans-serif; font-size: 20px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
+      I\'m getting hungry
+  </td>
+  </tr>
+  <tr style="background-color:#000;">
+    <td style="padding: 10px 25px 25px 25px; text-align: left; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
       Bathe private parts with tongue then lick owner\'s face sniff other cat\'s butt and hang jaw half open thereafter. Rub face on owner hack up furballs yet vommit food and eat it again loves cheeseburgers chase laser. Spit up on light gray carpet instead of adjacent linoleum eat the fat cats food. Scamper. Stare at ceiling light meowzer! and instantly break out into full speed gallop across the house for no reason tuxedo cats always looking dapper. Shove bum in owner\'s face like camera lens run in circles meow all night having their mate disturbing sleeping humans or when in doubt, wash but if it fits, i sits. Plan steps for world domination scamper.
         <br><br>
         <!--  BEGIN BUTTON -->
