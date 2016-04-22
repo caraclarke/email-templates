ebc47598279b879554d97eda56898a487e9a4ec8
caraclarke <cara.clarke8@gmail.com>
2016-04-22 11:43:02 -0400

    Spacing changed for layout-two pieces so they are easier to read

--- main-layout-two.php
+++ main-layout-two.php
@@ -1,60 +1,60 @@
 <?php
-  echo '<tr>
-      <td align="center" valign="top" style="padding: 10px;">
-          <table cellspacing="0" cellpadding="0" border="0" width="100%">
+  echo '
+  <tr>
+    <td align="center" valign="top" style="padding: 10px;">
+      <table cellspacing="0" cellpadding="0" border="0" width="100%">
+        <tr>
+          <!-- Column : BEGIN -->
+          <td class="stack-column-center">
+            <table cellspacing="0" cellpadding="0" border="0">
               <tr>
-                  <!-- Column : BEGIN -->
-                  <td class="stack-column-center">
-                      <table cellspacing="0" cellpadding="0" border="0">
-                          <tr>
-                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 10px; text-align: left;" class="center-on-narrow">
-                              <h1>heyhey</h1>
-                                  Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-                                  <br><br>
-                                  <!--  BEGIN BUTTON -->
-                                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
-                                      <tr>
-                                          <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
-                                              <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
-                                          </td>
-                                      </tr>
-                                  </table>
-                                  <!-- END BUTTON -->
-                              </td>
-                          </tr>
-                          
-                          <tr>
-                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 10px; text-align: left;" class="center-on-narrow">
-                              <h1>heyhey</h1>
-                                  Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-                                  <br><br>
-                                  <!--  BEGIN BUTTON -->
-                                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
-                                      <tr>
-                                          <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
-                                              <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
-                                          </td>
-                                      </tr>
-                                  </table>
-                                  <!-- END BUTTON -->
-                              </td>
-                          </tr>
-                      </table>
-                  </td>
-                  <!-- Column : END -->
-                  <!-- Column : BEGIN -->
-                  <td class="stack-column-center">
-                      <table cellspacing="0" cellpadding="0" border="0">
-                          <tr>
-                              <td style="padding: 10px; text-align: center">
-                                  <img src="http://placekitten.com/290/475" width="290" height="475" alt="alt_text" border="0" class="fluid">
-                              </td>
-                          </tr>
-                      </table>
-                  </td>
-                  <!-- Column : END -->
+                <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 10px; text-align: left;" class="center-on-narrow">
+                  <h1>heyhey</h1>
+                  <p>Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                  <!--  BEGIN BUTTON -->
+                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
+                    <tr>
+                      <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+                          <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
+                      </td>
+                    </tr>
+                  </table>
+                  <!-- END BUTTON -->
+                </td>
               </tr>
-          </table>
-      </td>
-  </tr>';
+            
+              <tr>
+                <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 10px; text-align: left;" class="center-on-narrow">
+                  <h1>heyhey</h1>
+                  <p>Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                  <!--  BEGIN BUTTON -->
+                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
+                    <tr>
+                      <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+                          <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
+                      </td>
+                    </tr>
+                  </table>
+                  <!-- END BUTTON -->
+                </td>
+              </tr>
+            </table>
+          </td>
+          <!-- Column : END -->
+          <!-- Column : BEGIN -->
+          <td class="stack-column-center">
+            <table cellspacing="0" cellpadding="0" border="0">
+              <tr>
+                <td style="padding: 10px; text-align: center">
+                  <img src="http://placekitten.com/290/475" width="290" height="475" alt="alt_text" border="0" class="fluid">
+                </td>
+              </tr>
+            </table>
+          </td>
+          <!-- Column : END -->
+        </tr>
+      </table>
+    </td>
+  </tr>
+';
 ?>
--- two-cols.php
+++ two-cols.php
@@ -1,60 +1,62 @@
 <?php
-  echo '<tr>
-      <td align="center" valign="top" style="padding: 10px;">
-          <table cellspacing="0" cellpadding="0" border="0" width="100%">
+  echo '
+  <tr>
+    <td align="center" valign="top" style="padding: 10px;">
+      <table cellspacing="0" cellpadding="0" border="0" width="100%">
+        <tr>
+          <!-- Column : BEGIN -->
+          <td class="stack-column-center">
+            <table cellspacing="0" cellpadding="0" border="0">
               <tr>
-                  <!-- Column : BEGIN -->
-                  <td class="stack-column-center">
-                      <table cellspacing="0" cellpadding="0" border="0">
-                          <tr>
-                              <td class="imagePaddingMin" style="padding: 10px 10px 10px 0px; text-align: center">
-                                  <img src="http://placekitten.com/270/" width="100%" height="270" alt="alt_text" border="0" class="fluid">
-                              </td>
-                          </tr>
-                          <tr>
-                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 0px; text-align: center;" class="center-on-narrow paddingMin">
-                                <h3 style="margin:5px 5px 5px 0px;">HEader</h3>
-                                  <!--  BEGIN BUTTON -->
-                                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
-                                      <tr>
-                                          <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
-                                              <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
-                                          </td>
-                                      </tr>
-                                  </table>
-                                  <!-- END BUTTON -->
-                              </td>
-                          </tr>
-                      </table>
-                  </td>
-                  <!-- Column : END -->
-                  <!-- Column : BEGIN -->
-                  <td class="stack-column-center">
-                      <table cellspacing="0" cellpadding="0" border="0">
-                          <tr>
-                              <td class="imagePaddingMin" style="padding: 10px 0px 10px 10px; text-align: center">
-                                  <img src="http://placekitten.com/270/" width="100%" height="270" alt="alt_text" border="0" class="fluid">
-                              </td>
-                          </tr>
-                          <tr>
-                              <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 0px 10px 10px; text-align: center;" class="center-on-narrow paddingMin">
-                                <h3 style="margin:5px 5px 5px 0px;">HEader</h3>
-                                  <!--  BEGIN BUTTON -->
-                                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
-                                      <tr>
-                                          <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
-                                              <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
-                                          </td>
-                                      </tr>
-                                  </table>
-                                  <!-- END BUTTON -->
-                              </td>
-                          </tr>
-                      </table>
-                  </td>
-                  <!-- Column : END -->
+                <td class="imagePaddingMin" style="padding: 10px 10px 10px 0px; text-align: center">
+                  <img src="http://placekitten.com/270/" width="100%" height="270" alt="alt_text" border="0" class="fluid">
+                </td>
               </tr>
-          </table>
-      </td>
-  </tr>';
+              <tr>
+                <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 10px 10px 0px; text-align: center;" class="center-on-narrow paddingMin">
+                  <h3 style="margin:5px 5px 5px 0px;">HEader</h3>
+                  <!--  BEGIN BUTTON -->
+                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
+                    <tr>
+                      <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+                        <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
+                      </td>
+                    </tr>
+                  </table>
+                  <!-- END BUTTON -->
+                </td>
+              </tr>
+            </table>
+          </td>
+          <!-- Column : END -->
+          <!-- Column : BEGIN -->
+          <td class="stack-column-center">
+            <table cellspacing="0" cellpadding="0" border="0">
+              <tr>
+                <td class="imagePaddingMin" style="padding: 10px 0px 10px 10px; text-align: center">
+                  <img src="http://placekitten.com/270/" width="100%" height="270" alt="alt_text" border="0" class="fluid">
+                </td>
+              </tr>
+              <tr>
+                <td style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555; padding: 0px 0px 10px 10px; text-align: center;" class="center-on-narrow paddingMin">
+                  <h3 style="margin:5px 5px 5px 0px;">HEader</h3>
+                  <!--  BEGIN BUTTON -->
+                  <table border="0" cellpadding="0" cellspacing="0" class="emailButton" style="border-radius:3px; background-color:#6DC6DD; width:100%;">
+                    <tr>
+                      <td align="center" align="middle" class="emailButtonContent" style="padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+                        <a href="..." target="_blank" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">Click Me</a>
+                      </td>
+                    </tr>
+                  </table>
+                  <!-- END BUTTON -->
+                </td>
+              </tr>
+            </table>
+          </td>
+          <!-- Column : END -->
+        </tr>
+      </table>
+    </td>
+  </tr>
+';
 ?>
