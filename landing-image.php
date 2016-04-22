13a4560f9958702ea102fbddf07a9237967670bb
caraclarke <cara.clarke8@gmail.com>
2016-04-22 08:28:12 -0400

    button for landing image width and color change

--- landing-image.php
+++ landing-image.php
@@ -7,15 +7,15 @@ echo '<tr>
 <!-- Hero Image, Flush : END -->
 
 <!-- 1 Column Text : BEGIN -->
-<tr>
-    <td style="padding: 40px; text-align: center; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #000;">
+<tr style="background-color:#000;">
+    <td style="padding: 40px; text-align: center; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #fff;">
         Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.
         <br><br>
         <!-- Button : Begin -->
-        <table cellspacing="0" cellpadding="0" border="0" align="center" style="Margin: auto">
+        <table cellspacing="0" cellpadding="0" border="0" align="center" style="Margin: auto; width:60%; background-color: gray;">
             <tr>
-                <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
-                    <a href="http://www.google.com" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                <td style="border-radius: 3px; background: gray; text-align: center;" class="button-td">
+                    <a href="http://www.google.com" style="background: gray; border: 15px solid gray; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
                         &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">A Button</span>&nbsp;&nbsp;&nbsp;&nbsp;
                     </a>
                 </td>
