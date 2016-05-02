2908b1e1bc0da894f6260aad798dabfb6909d964
caraclarke <cara.clarke8@gmail.com>
2016-05-02 11:54:37 -0400

    html pages updated with mktedtiable and custom ids

--- no-mq-partials/header-two-nmq.php
+++ no-mq-partials/header-two-nmq.php
@@ -5,7 +5,7 @@ echo '
       <table align="left" width="100%" style="border-spacing:0; color:#333333;" >
         <tr>
           <td class="inner contents" style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;width:100%;text-align:left;" >
-            <img align="absbottom" src="http://go.codiscope.com/rs/442-XVH-714/images/color-logo.svg" height="auto" width="200" alt="" style="border-width:0;width:100%;height:auto;max-width:200px;" />
+            <div class="mktEditable" id="headerTwoLogo"><img align="absbottom" src="http://go.codiscope.com/rs/442-XVH-714/images/color-logo.svg" height="auto" width="200" alt="" style="border-width:0;width:100%;height:auto;max-width:200px;" /></div>
           </td>
         </tr>
       </table>
@@ -14,18 +14,18 @@ echo '
 
   <tr>
      <td style="Padding-top:3px;Padding-bottom:3px;Padding-right:3px;Padding-left:3px;width:100%;text-align:center;">
-        <p class="h1" style="font-size: 30px; margin:5px; text-transform:uppercase; line-height:33px;">Pelt around the house.</p>
+        <div class="mktEditable" id="layoutTwoHeader"><p class="h1" style="font-size: 30px; margin:5px; text-transform:uppercase; line-height:33px;">Pelt around the house.</p></div>
      </td>
    </tr>
    <tr>
     <td>
-      <p style="text-align:center;"><img align="middle" src="http://go.codiscope.com/rs/442-XVH-714/images/header-divider.png" height="auto" width="600" alt="alt_text" style="width:100%;height:auto;max-width:600px;" /></p>
+      <div class="mktEditable" id="layoutTwoHeaderDivider"><p style="text-align:center;"><img align="middle" src="http://go.codiscope.com/rs/442-XVH-714/images/header-divider.png" height="auto" width="600" alt="alt_text" style="width:100%;height:auto;max-width:600px;" /></p></div>
     </td>
    </tr>
 
    <tr>
      <td class="leftOnMin" style="Padding: 25px 5px; text-align: left; mso-height-rule: exactly; line-height: 20px; color: #000;">
-      All of a sudden cat goes crazy pee in human\'s bed until he cleans the litter box burrow under covers climb leg. Sun bathe cats go for world domination, the dog smells bad. Chase imaginary bugs chase the pig around the house for white cat sleeps on a black shirt and sit on the laptop.
+      <div class="mktEditable" id="layoutTwoSubText">All of a sudden cat goes crazy pee in human\'s bed until he cleans the litter box burrow under covers climb leg. Sun bathe cats go for world domination, the dog smells bad. Chase imaginary bugs chase the pig around the house for white cat sleeps on a black shirt and sit on the laptop.</div>
      </td>
    </tr>
 ';
