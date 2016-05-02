6b64e0e863d11257c441b456163b02846ccfa6bf
caraclarke <cara.clarke8@gmail.com>
2016-05-02 15:32:41 -0400

    testing if fixes mobile

--- no-mq-partials/two-cols-nmq.php
+++ no-mq-partials/two-cols-nmq.php
@@ -8,7 +8,7 @@ echo '
         <td width="50%" valign="top" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" >
         <![endif]-->
         <div class="column" style="height:100%;position:relative;width:100%;max-width:300px;display:inline-block;vertical-align:top;" >
-            <table width="100%" style="border-spacing:0; color:#333333;" >
+            <table border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" style="border-spacing:0; color:#333333;" >
                 <tr>
                     <td class="inner" style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;" >
                         <table class="contents" style="border-spacing:0; color:#333333;width:100%;font-size:14px;text-align:left;" >
@@ -45,7 +45,7 @@ echo '
         </td><td width="50%" valign="top" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" >
         <![endif]-->
         <div class="column" style="height:100%;position:relative;width:100%;max-width:300px;display:inline-block;vertical-align:top;" >
-            <table width="100%" style="border-spacing:0; color:#333333;" >
+            <table border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" style="border-spacing:0; color:#333333;" >
                 <tr>
                     <td class="inner" style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;" >
                         <table class="contents" style="border-spacing:0; color:#333333;width:100%;font-size:14px;text-align:left;" >
